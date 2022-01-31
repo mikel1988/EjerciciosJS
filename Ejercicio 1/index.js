a4e6860bfd17c05350d14ce89286eeb7c7e21620
mikel1988 <97702863+mikel1988@users.noreply.github.com>
2022-01-31 20:09:24 +0100

    Ejercicio 1

--- Ejercicio 1/index.js
+++ Ejercicio 1/index.js
@@ -11,3 +11,13 @@ const areaBase = 16*23;
 const areaTotal = areaLateral + 2*areaBase;
 console.log('areaTotal', areaTotal + ' cm2');
 
+// Generalización
+const base = 23;
+const altura = 13;
+const ancho = 16;
+
+const volumen2 = base * altura * ancho;
+console.log('volumen2', volumen2 + ' cm3')
+
+const area2 = base * altura + 2 * base * ancho;
+console.log('area2', area2 + 'cm2')
